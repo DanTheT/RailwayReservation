66b1b106cdb6d1eb564c30f7e6dfc92ac5ef20b5
Dan <danzack763@gmail.com>
2022-01-26 20:35:30 +0800

    Forth

--- app/src/main/java/com/example/railwayreservation/MainActivity.kt
+++ app/src/main/java/com/example/railwayreservation/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.railwayreservation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.railwayreservation.admin.AdminLogin
+import com.example.railwayreservation.databinding.ActivityMainBinding
+import com.example.railwayreservation.passenger.PassengerHome
+import com.example.railwayreservation.passenger.PassengerLogin
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.buttonPassengerLogin.setOnClickListener {
+            val intent = Intent(this, PassengerLogin::class.java).apply {
+
+            }
+            startActivity(intent)
+        }
+
+        binding.buttonAdminLogin.setOnClickListener {
+            val intent = Intent(this, AdminLogin::class.java).apply {
+
+            }
+            startActivity(intent)
+        }
     }
 }
