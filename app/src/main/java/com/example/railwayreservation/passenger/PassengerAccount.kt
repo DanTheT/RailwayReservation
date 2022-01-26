98178806d3ecd7b1a91c7a5563d6e34fda41edef
Dan <danzack763@gmail.com>
2022-01-26 19:50:25 +0800

    Forth

---
+++ app/src/main/java/com/example/railwayreservation/passenger/PassengerAccount.kt
@@ -0,0 +1,12 @@
+package com.example.railwayreservation.passenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.railwayreservation.R
+
+class PassengerAccount : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_passenger_account)
+    }
+}
---
+++ app/src/main/java/com/example/railwayreservation/passenger/PassengerSignup.kt
@@ -0,0 +1,12 @@
+package com.example.railwayreservation.passenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.railwayreservation.R
+
+class PassengerSignup : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_passenger_signup)
+    }
+}
