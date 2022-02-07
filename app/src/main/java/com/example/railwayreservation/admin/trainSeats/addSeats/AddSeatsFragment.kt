853cc55ec59d61c4887dce38a68ad248234c3ed5
Dan <danzack763@gmail.com>
2022-02-08 00:08:19 +0800

    Committed new adjustments

--- app/src/main/java/com/example/railwayreservation/admin/trainSeats/addSeats/AddSeatsFragment.kt
+++ app/src/main/java/com/example/railwayreservation/admin/trainSeats/addSeats/AddSeatsFragment.kt
@@ -8,6 +8,8 @@ import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
@@ -24,9 +26,11 @@ class AddSeatsFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var seatsDatabase: DatabaseReference
     private lateinit var navController: NavController
+    private lateinit var addViewModel: AddSeatsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        addViewModel = ViewModelProvider(this)[AddSeatsViewModel::class.java]
     }
 
     override fun onCreateView(
@@ -45,6 +49,7 @@ class AddSeatsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         navController = Navigation.findNavController(view)
 
         binding.addTrainSeatsMainTopAppBar.setOnClickListener {
@@ -52,15 +57,36 @@ class AddSeatsFragment : Fragment() {
         }
 
         binding.buttonCheckCategory.setOnClickListener {
+            val trainName = binding.editTextSeatTrainName.text.toString()
+
+            seatsDatabase = FirebaseDatabase.getInstance().getReference("SpecificTrainInfo")
+            seatsDatabase.child(trainName).child("car").get().addOnSuccessListener {
+
+                when(it.value.toString()) {
+                    "4 Coaches" -> {
+                        insertSeatsCoachesFour()
+                    }
+                    "5 Coaches" -> {
+                        insertSeatsCoachesFive()
+                    }
+                }
+            }
+
             when(binding.editTextSeatCategory.text.toString()) {
                 "VIP seats" -> {
                     insertSeatsRange()
+                    binding.editTextSeatSelectionStandardWindow.text = null
+                    binding.editTextSeatSelectionStandardAlley.text = null
                 }
                 "Standard seats - At Window" -> {
                     insertSeatsRange2()
+                    binding.editTextSeatSelection.text = null
+                    binding.editTextSeatSelectionStandardAlley.text = null
                 }
                 else -> {
                     insertSeatsRange3()
+                    binding.editTextSeatSelection.text = null
+                    binding.editTextSeatSelectionStandardWindow.text = null
                 }
             }
         }
@@ -108,22 +134,97 @@ class AddSeatsFragment : Fragment() {
         binding.editTextSeatSelectionStandardWindow.setAdapter(null)
     }
 
+    private fun insertSeatsCoachesFour() {
+        val lists = resources.getStringArray(R.array.four_coaches)
+
+        val listsAdapter = ArrayAdapter(requireContext(), R.layout.list_for_dropdown, lists)
+        binding.editTextSeatCoachSelection.setAdapter(listsAdapter)
+    }
+
+    private fun insertSeatsCoachesFive() {
+        val lists = resources.getStringArray(R.array.five_coaches)
+
+        val listsAdapter = ArrayAdapter(requireContext(), R.layout.list_for_dropdown, lists)
+        binding.editTextSeatCoachSelection.setAdapter(listsAdapter)
+    }
+
     private fun insertSeatData() {
         val trainName = binding.editTextSeatTrainName.text.toString()
+        val trainCoach = binding.editTextSeatCoachSelection.text.toString()
         val available = "Yes"
         val reserved = "No"
         val seatPrice = binding.editTextSeatTrainPrice.text.toString()
 
         seatsDatabase = FirebaseDatabase.getInstance().getReference("TrainSeats")
 
+        // for vip seats
         when (binding.editTextSeatSelection.text.toString()){
             "A1 - D1" -> {
                 val arrayList = arrayListOf<String>("A1", "B1", "C1", "D1")
-                val seats = SeatsData (
-                    available, arrayList, reserved, seatPrice
-                        )
+
                 for (i in 0..3) {
-                    seatsDatabase.child(trainName).child(arrayList[i]).setValue(seats)
+                    val seats = SeatsData (
+                        available, arrayList[i], reserved, seatPrice
+                    )
+                    seatsDatabase.child(trainName).child(trainCoach).child(arrayList[i]).setValue(seats)
+                }
+            }
+            "A2 - D2" -> {
+                val arrayList = arrayListOf<String>("A2", "B2", "C2", "D2")
+
+                for (i in 0..3) {
+                    val seats = SeatsData (
+                        available, arrayList[i], reserved, seatPrice
+                    )
+                    seatsDatabase.child(trainName).child(trainCoach).child(arrayList[i]).setValue(seats)
+                }
+            }
+        }
+
+        // for window standard
+        when (binding.editTextSeatSelectionStandardWindow.text.toString()) {
+            "A3 - A13" -> {
+                val arrayList = arrayListOf<String>("A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10", "A11", "A12", "A13")
+
+                for (i in 0..10) {
+                    val seats = SeatsData (
+                        available, arrayList[i], reserved, seatPrice
+                    )
+                    seatsDatabase.child(trainName).child(trainCoach).child(arrayList[i]).setValue(seats)
+                }
+            }
+            "D3 - D13" -> {
+                val arrayList = arrayListOf<String>("D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10", "D11", "D12", "D13")
+
+                for (i in 0..10) {
+                    val seats = SeatsData (
+                        available, arrayList[i], reserved, seatPrice
+                    )
+                    seatsDatabase.child(trainName).child(trainCoach).child(arrayList[i]).setValue(seats)
+                }
+            }
+        }
+
+        //for standard alley seats
+        when (binding.editTextSeatSelectionStandardAlley.text.toString()) {
+            "B3 - B13" -> {
+                val arrayList = arrayListOf<String>("B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10", "B11", "B12", "B13")
+
+                for (i in 0..10) {
+                    val seats = SeatsData (
+                        available, arrayList[i], reserved, seatPrice
+                    )
+                    seatsDatabase.child(trainName).child(trainCoach).child(arrayList[i]).setValue(seats)
+                }
+            }
+            "C3 - C13" -> {
+                val arrayList = arrayListOf<String>("C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10", "C11", "C12", "C13")
+
+                for (i in 0..10) {
+                    val seats = SeatsData (
+                        available, arrayList[i], reserved, seatPrice
+                    )
+                    seatsDatabase.child(trainName).child(trainCoach).child(arrayList[i]).setValue(seats)
                 }
             }
         }
