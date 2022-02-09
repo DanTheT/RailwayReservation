be69ce83fcc0146bdcc5aced545dd835e545cdff
Dan <danzack763@gmail.com>
2022-02-09 19:43:32 +0800

    Committed new adjustments

--- app/src/main/java/com/example/railwayreservation/admin/trainSeats/OverallTrainSeatsFragment.kt
+++ app/src/main/java/com/example/railwayreservation/admin/trainSeats/OverallTrainSeatsFragment.kt
@@ -5,24 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.railwayreservation.R
 import com.example.railwayreservation.databinding.FragmentOverallTrainSeatsBinding
+import com.google.firebase.database.*
 
 class OverallTrainSeatsFragment : Fragment() {
 
     private var _binding: FragmentOverallTrainSeatsBinding? = null
     private val binding get() = _binding!!
     private lateinit var navController: NavController
+    private lateinit var seatsRecycleView: RecyclerView
+    private lateinit var seatsArrayList: ArrayList<SeatsData>
+    private lateinit var seatsDb: DatabaseReference
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentOverallTrainSeatsBinding.inflate(inflater, container, false)
+        seatsRecycleView = binding.displayOverallSeatsRV
+        seatsRecycleView.layoutManager = LinearLayoutManager(context)
+        seatsArrayList = arrayListOf()
+
+        insertTrainName()
+
+        try {
+            binding.searchSeatName.setOnClickListener {
+                val name = binding.textFieldSearchSeatName.text.toString()
+                getSeatsData(name)
+            }
+        }catch (e: Exception) {
+            e.message
+        }
+
+        binding.clearRecyclerSeatName.setOnClickListener {
+            try {
+                if (seatsRecycleView.isShown) {
+                    seatsArrayList.clear()
+                    seatsRecycleView.adapter?.notifyDataSetChanged()
+                }
+            }catch (e: Exception) {
+                e.message
+            }
+        }
+
         return binding.root
     }
 
@@ -36,6 +68,35 @@ class OverallTrainSeatsFragment : Fragment() {
 
         binding.fltAddNewSeats.setOnClickListener {
             findNavController().navigate(R.id.action_overallTrainSeatsFragment_to_addSeatsFragment)
+        }
+    }
+
+    private fun insertTrainName() {
+        val lists = resources.getStringArray(R.array.train_name_items)
+
+        val listAdapter = ArrayAdapter(requireContext(), R.layout.list_for_dropdown, lists)
+        binding.textFieldSearchSeatName.setAdapter(listAdapter)
+    }
+
+    private fun getSeatsData(trainName: String) {
+        seatsDb = FirebaseDatabase.getInstance().getReference("TrainSeats")
+        for (i in 1..4) {
+            seatsDb.child(trainName).child("Coach $i").addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        for (seatSnap in snapshot.children) {
+                            val seats = seatSnap.getValue(SeatsData::class.java)
+                            seatsArrayList.add(seats!!)
+                        }
+                        seatsRecycleView.adapter = SeatsAdapter(seatsArrayList)
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+            })
         }
     }
 
---
+++ app/src/main/java/com/example/railwayreservation/admin/trainSeats/SeatsAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.railwayreservation.admin.trainSeats
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.railwayreservation.R
+
+class SeatsAdapter(private val seatsList: ArrayList<SeatsData>): RecyclerView.Adapter<SeatsAdapter.SeatsViewHolder>() {
+    class SeatsViewHolder(seatsView: View): RecyclerView.ViewHolder(seatsView) {
+        val seatAvailable: TextView = seatsView.findViewById(R.id.textViewAvailable)
+        val seatNumber: TextView = seatsView.findViewById(R.id.textViewSeatNo)
+        val seatReserved: TextView = seatsView.findViewById(R.id.textViewSeatReserve)
+        val seatFare: TextView = seatsView.findViewById(R.id.textViewSeatFare)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SeatsViewHolder {
+        val seatView = LayoutInflater.from(parent.context).inflate(R.layout.list_seats_admin, parent, false)
+        return SeatsViewHolder(seatView)
+    }
+
+    override fun onBindViewHolder(holder: SeatsViewHolder, position: Int) {
+        val seats: SeatsData = seatsList[position]
+        holder.seatAvailable.text = seats.available
+        holder.seatNumber.text = seats.seatNo
+        holder.seatReserved.text = seats.reserved
+        holder.seatFare.text = seats.seatPrice
+    }
+
+    override fun getItemCount(): Int {
+        return seatsList.size
+    }
+}
