92fc99cf064f1dce709cec8120852dbb8ab8b310
Dan <danzack763@gmail.com>
2022-02-23 00:21:09 +0800

    Committed new adjustments

--- app/src/main/java/com/example/railwayreservation/passengerTrain/trainSeats/PassengerSeatsFragment.kt
+++ app/src/main/java/com/example/railwayreservation/passengerTrain/trainSeats/PassengerSeatsFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.navArgs
+import com.example.railwayreservation.R
 import com.example.railwayreservation.databinding.FragmentPassengerSeatsBinding
 import com.google.firebase.database.*
 
@@ -36,25 +38,36 @@ class PassengerSeatsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        try {
-            getSelectedSeatData(binding.seatTrainType.text.toString())
-        }catch (e: Exception) {
-            Toast.makeText(requireContext(), e.message, Toast.LENGTH_SHORT).show()
+        binding.selectSeatGetBtn.setOnClickListener {
+            val getNameTrain = binding.seatTrainType.text.toString()
+
+            seatsDatabase = FirebaseDatabase.getInstance().getReference("SpecificTrainInfo")
+            seatsDatabase.child(getNameTrain).child("car").get().addOnSuccessListener {
+
+                when(it.value.toString()) {
+                    "4 Coaches" -> {
+                        insertSeatsCoachesFour()
+                    }
+                    "5 Coaches" -> {
+                        insertSeatsCoachesFive()
+                    }
+                }
+            }
         }
     }
 
-    private fun getSelectedSeatData(trainType: String) {
-        seatsDatabase =
-            FirebaseDatabase.getInstance().getReference("TrainInfo").child(trainType).child("Seats")
+    private fun insertSeatsCoachesFive() {
+        val lists = resources.getStringArray(R.array.four_coaches)
 
-        seatsDatabase.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-
-            }
-        })
+        val listsAdapter = ArrayAdapter(requireContext(), R.layout.list_for_dropdown, lists)
+        binding.selectSeatCoach.setAdapter(listsAdapter)
     }
+
+    private fun insertSeatsCoachesFour() {
+        val lists = resources.getStringArray(R.array.four_coaches)
+
+        val listsAdapter = ArrayAdapter(requireContext(), R.layout.list_for_dropdown, lists)
+        binding.selectSeatCoach.setAdapter(listsAdapter)
+    }
+
 }
